efb6edcf6f5a7ff3451c506dec39c53dde4efc71
Fabian Jose Roldan Pineros <fjroldan@akenfactory.com>
2023-02-17 17:46:40 -0500

    Add piepeline

--- pipelines/basic_deploy.groovy
+++ pipelines/basic_deploy.groovy
@@ -39,7 +39,7 @@ pipeline{
                     // upload package
                     // sh "curl -T TodoApi.tar ftps://waws-prod-am2-533.ftp.azurewebsites.windows.net -u '$ftpProfile.username:$ftpProfile.password'"
 
-                    sh "curl -T TodoApi.tar -k "sftp://waws-prod-am2-533.ftp.azurewebsites.windows.net/site/wwwroot" -u '$ftpProfile.username:$ftpProfile.password'"
+                    sh "curl -T TodoApi.tar -k 'sftp://waws-prod-am2-533.ftp.azurewebsites.windows.net/site/wwwroot' -u '$ftpProfile.username:$ftpProfile.password'"
 
 
                     /*sh '''
